caf5b17e4f2be9c7a8a32c59be9917c63632ece6
ejcondorf88 <eliancondor2523658@gmail.com>
2026-03-30 09:13:48 -0500

    feat: ❌ remove Allure integration

--- src/test/java/TestRunner.java
+++ src/test/java/TestRunner.java
@@ -1,6 +1,5 @@
 import com.intuit.karate.Results;
 import com.intuit.karate.Runner;
-import io.qameta.allure.karate.AllureKarate;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
@@ -9,8 +8,7 @@ class TestRunner {
     @Test
     void testParallel() {
         Results results = Runner.path("classpath:users", "classpath:products")
-                .hook(new AllureKarate())
-                .parallel(1);
+            .parallel(1);
         assertEquals(0, results.getFailCount(), results.getErrorMessages());
     }
 
--- src/test/java/products/ProductsRunner.java
+++ src/test/java/products/ProductsRunner.java
@@ -1,13 +1,12 @@
 package products;
 
 import com.intuit.karate.junit5.Karate;
-import io.qameta.allure.karate.AllureKarate;
 
 class ProductsRunner {
 
     @Karate.Test
     Karate testProducts() {
-        return Karate.run("products").hook(new AllureKarate()).relativeTo(getClass());
+        return Karate.run("products").relativeTo(getClass());
     }
 
 }
